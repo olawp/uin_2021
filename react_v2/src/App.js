cf381b1509a4764cd0f5165b9288c1d88ef0bfed
Ola Wethal Petersen <olawp1997@gmail.com>
2021-03-03 11:56:47 +0100

    Converted task to react, need button functionality

---
+++ react_v2/src/App.js
@@ -0,0 +1,17 @@
+import './styles.scss'
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+import Todogrid from './components/Todogrid'
+
+
+const App = () => (
+  <>
+
+      <Navbar />
+      <Form />
+      <Todogrid />
+
+      </>
+  );
+
+export default App;
